26d0861781fcfffab9067db010db0cce89ba4510
lemii <25596204+Lemii@users.noreply.github.com>
2023-08-09 12:00:11 +0200

    fix landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import Redirect from 'components/ui/Redirect';
+import Spinner from 'components/ui/Spinner';
 import { useAccountMapEntry } from 'hooks/useAccountMapEntry';
 
 const LandingPage = () => {
   const { user } = useAuth0();
-  const { map } = useAccountMapEntry(user?.email);
+  const { map, loading } = useAccountMapEntry(user?.email);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   if (!map) {
     return <Redirect to="/register" />;
