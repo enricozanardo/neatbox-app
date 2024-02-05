b767305f8a2e1589a758a16556ddfc94b6e5bf03
lemii <25596204+Lemii@users.noreply.github.com>
2024-02-05 11:14:37 +0100

    implement "pending" / neutral client status

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,8 @@ import useAccountData from 'hooks/useAccountData';
 import BrowsePage from 'pages/BrowsePage';
 import ForbiddenPage from 'pages/ForbiddenPage';
 import LandingPage from 'pages/LandingPage';
+import LoadingPage from 'pages/LoadingPage';
+import ServiceUnavailable from 'pages/ServiceUnavailablePage';
 import ViewPage from 'pages/ViewPage';
 import WelcomePage from 'pages/WelcomePage';
 import { lazy, Suspense } from 'react';
@@ -13,7 +15,6 @@ const CollectionsPage = lazy(() => import('pages/CollectionsPage'));
 const DashboardPage = lazy(() => import('pages/DashboardPage'));
 const HomePage = lazy(() => import('pages/HomePage'));
 const UnauthorizedPage = lazy(() => import('pages/UnauthorizedPage'));
-const ServiceUnavailablePage = lazy(() => import('pages/ServiceUnavailablePage'));
 const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 const RequestsPage = lazy(() => import('pages/RequestsPage'));
 const TransferCollectionPage = lazy(() => import('pages/TransferCollectionPage'));
@@ -21,14 +22,6 @@ const TransferFilePage = lazy(() => import('pages/TransferFilePage'));
 const TransferPage = lazy(() => import('pages/TransferPage'));
 const UploadPage = lazy(() => import('pages/UploadPage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage'));
-
-const DisconnectedClientFallback = ({ isConnected, children }: { isConnected: boolean; children?: any }) => {
-  if (!isConnected) {
-    return <ServiceUnavailablePage />;
-  }
-
-  return children ?? <Outlet />;
-};
 
 const AuthenticatedRoute = ({ isAllowed, children }: { isAllowed: boolean; children?: any }) => {
   if (!isAllowed) {
@@ -38,7 +31,23 @@ const AuthenticatedRoute = ({ isAllowed, children }: { isAllowed: boolean; child
   return children ?? <Outlet />;
 };
 
-const RegisteredUserRoute = ({ isAllowed, children }: { isAllowed: boolean; children?: any }) => {
+const RegisteredUserRoute = ({
+  isAllowed,
+  clientIsOnline,
+  children,
+}: {
+  isAllowed: boolean;
+  clientIsOnline: boolean | null;
+  children?: any;
+}) => {
+  if (!isAllowed && clientIsOnline === null) {
+    return <LoadingPage />;
+  }
+
+  if (!isAllowed && clientIsOnline === false) {
+    return <ServiceUnavailable />;
+  }
+
   if (!isAllowed) {
     return <ForbiddenPage />;
   }
@@ -66,15 +75,13 @@ const Routes = () => {
         <Route element={<AuthenticatedRoute isAllowed={isAuthenticated} />}>
           <Route path="/dashboard" element={<DashboardPage />} />
 
-          <Route element={<DisconnectedClientFallback isConnected={clientIsOnline} />}>
-            <Route element={<RegisteredUserRoute isAllowed={accountExists} />}>
-              <Route path="/upload" element={<UploadPage />} />
-              <Route path="/transfer" element={<TransferPage />} />
-              <Route path="/transfer/file" element={<TransferFilePage />} />
-              <Route path="/transfer/collection" element={<TransferCollectionPage />} />
-              <Route path="/collections" element={<CollectionsPage />} />
-              <Route path="/requests" element={<RequestsPage />} />
-            </Route>
+          <Route element={<RegisteredUserRoute clientIsOnline={clientIsOnline} isAllowed={accountExists} />}>
+            <Route path="/upload" element={<UploadPage />} />
+            <Route path="/transfer" element={<TransferPage />} />
+            <Route path="/transfer/file" element={<TransferFilePage />} />
+            <Route path="/transfer/collection" element={<TransferCollectionPage />} />
+            <Route path="/collections" element={<CollectionsPage />} />
+            <Route path="/requests" element={<RequestsPage />} />
           </Route>
         </Route>
       </BrowserRoutes>
--- src/stores/useClientStatusStore.ts
+++ src/stores/useClientStatusStore.ts
@@ -1,11 +1,11 @@
 import { create } from 'zustand';
 
 type ClientStatusStoreState = {
-  clientIsOnline: boolean;
+  clientIsOnline: boolean | null;
   updateStatus: (value: boolean) => void;
 };
 
 export const useClientStatusStore = create<ClientStatusStoreState>(set => ({
-  clientIsOnline: false,
+  clientIsOnline: null,
   updateStatus: clientIsOnline => set(() => ({ clientIsOnline })),
 }));
